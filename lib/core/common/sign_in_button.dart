ca947dff4558540ac4677ad2ebed9d6806c49fbd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-01-31 13:12:37 +0100

    Update sign_in_button.dart

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../features/auth/controller/auth_controller.dart';
 import '../../theme/app_theme.dart';
 import '../constants/constants.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () async{
+          signInWithGoogle(ref);
+        },
         icon: Image.asset(
           Constants.googlePath,
           width: 35,
         ),
         label: const Text(
           'Continue with Google',
-          style: TextStyle(
-            fontSize: 18,
-            color: AppTheme.whiteColor
-          ),
+          style: TextStyle(fontSize: 18, color: AppTheme.whiteColor),
         ),
         style: ElevatedButton.styleFrom(
           backgroundColor: AppTheme.greyColor,
