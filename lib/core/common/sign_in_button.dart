b700052464f8d521bc18a635da5fc9bf654cae21
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-02 15:21:15 +0100

    Create sign_in_button.dart

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -8,32 +8,42 @@ import '../constants/constants.dart';
 class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.read(authControllerProvider).signInWithGoogle();
+  void signInWithGoogle(BuildContext context, WidgetRef ref) {
+    ref.read(authControllerProvider.notifier).signInWithGoogle(context);
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ElevatedButton.icon(
-        onPressed: () async{
-          signInWithGoogle(ref);
-        },
-        icon: Image.asset(
-          Constants.googlePath,
-          width: 35,
-        ),
-        label: const Text(
-          'Continue with Google',
-          style: TextStyle(fontSize: 18, color: AppTheme.whiteColor),
-        ),
+      child: ElevatedButton(
+        onPressed: () => signInWithGoogle(context, ref),
         style: ElevatedButton.styleFrom(
           backgroundColor: AppTheme.greyColor,
-          minimumSize: const Size(double.infinity, 48),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(20),
+            ),
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(Constants.googlePath, width: 35),
+                const SizedBox(width: 8.0),
+                const Text(
+                  'Continue with Google',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: AppTheme.whiteColor,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
