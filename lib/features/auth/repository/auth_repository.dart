61f0bc7880951bfcb716ec8f68e55728b1390571
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-02 15:22:26 +0100

    Create auth_repository.dart

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../../../core/constants/constants.dart';
+import '../../../core/constants/firebase_constants.dart';
+import '../../../core/failure.dart';
 import '../../../core/providers/firebase_providers.dart';
+import '../../../core/type_defs.dart';
+import '../../../models/user_model.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -27,18 +32,48 @@ class AuthRepository {
         _auth = auth,
         _googleSignIn = googleSignIn;
 
-  Future<void> signInWithGoogle() async {
+  CollectionReference get _users {
+    return _firestore.collection(FirebaseConstants.usersCollection);
+  }
+
+  FutureEither<UserModel> signInWithGoogle() async {
     try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final googleUser = await _googleSignIn.signIn();
       final googleAuth = await googleUser?.authentication;
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
       final userCredential = await _auth.signInWithCredential(credential);
-      debugPrint(userCredential.user?.displayName);
+
+      final UserModel user;
+
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        user = UserModel(
+          name: userCredential.user?.displayName ?? 'No name',
+          profilePicUrl:
+              userCredential.user?.photoURL ?? Constants.avatarDefault,
+          bannerUrl: Constants.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: [],
+        );
+        await _users.doc(userCredential.user!.uid).set(user.toMap());
+      } else {
+        user = await getUserData(userCredential.user!.uid).first;
+      }
+      return right(user);
+    } on FirebaseException catch (e) {
+      throw e.message!;
     } catch (e) {
-      debugPrint(e.toString());
+      return left(Failure(e.toString()));
     }
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _users.doc(uid).snapshots().map(
+          (event) => UserModel.fromMap(event.data() as Map<String, dynamic>),
+        );
   }
 }
