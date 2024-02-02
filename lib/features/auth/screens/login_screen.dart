a3a0858b54a4577193cbea5aafdc1c8e84902e16
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-02-02 15:22:38 +0100

    Create login_screen.dart

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit/core/common/loader.dart';
 
 import '../../../core/common/sign_in_button.dart';
 import '../../../core/constants/constants.dart';
+import '../controller/auth_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -27,7 +31,7 @@ class LoginScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
+      body: isLoading ? const Loader() : Column(
         children: [
           const SizedBox(height: 30),
           const Text(
